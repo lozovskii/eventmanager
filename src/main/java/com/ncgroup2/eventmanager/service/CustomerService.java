e4d76e1b7054fe22b5fff2a0bddb4cb4fba7cf4f
vv0rkman <b1kmoop@gmail.com>
2018-04-16 21:23:38 +0300

    Refactored renames

---
+++ src/main/java/com/ncgroup2/eventmanager/service/CustomerService.java
@@ -0,0 +1,55 @@
+package com.ncgroup2.eventmanager.service;
+
+import com.ncgroup2.eventmanager.entity.Customer;
+import com.ncgroup2.eventmanager.dao.impl.postgres.CustomerDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class CustomerService {
+    @Autowired
+    CustomerDaoImpl customerDaoImpl;
+
+    public Customer register(Customer customer) {
+
+        customerDaoImpl.addCustomer(customer);
+
+        return customerDaoImpl.getByField("token",customer.getToken());
+    }
+
+    public boolean isCustomerPresent(String login) {
+
+        return customerDaoImpl.getByField("login",login)!=null;
+    }
+
+    public void createVerificationToken(Customer customer, String token) {
+
+        customerDaoImpl.updateField(customer, "token",  token);
+    }
+
+    public boolean isEmailUnique(String email) {
+
+        return customerDaoImpl.getByField("email",email)==null;
+    }
+
+
+    public void deleteCustomer(Customer customer) {
+
+        customerDaoImpl.deleteCustomer(customer);
+    }
+
+    public Customer getCustomer(String token) {
+
+        return customerDaoImpl.getByField("token", token);
+    }
+
+    @Transactional
+    public void confirmCustomer(Customer customer) {
+
+        customerDaoImpl.updateField(customer,"isVerified", Boolean.TRUE);
+        customerDaoImpl.updateField(customer,"token","");
+    }
+
+}
