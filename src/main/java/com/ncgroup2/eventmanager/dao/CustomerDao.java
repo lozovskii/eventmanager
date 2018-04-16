8acd0bda4d027ab01e6cc8cca2d112d4acd78fb6
vv0rkman <b1kmoop@gmail.com>
2018-04-16 21:20:47 +0300

    New interface

---
+++ src/main/java/com/ncgroup2/eventmanager/dao/CustomerDao.java
@@ -0,0 +1,24 @@
+package com.ncgroup2.eventmanager.dao;
+
+import com.ncgroup2.eventmanager.entity.Customer;
+
+import java.util.Collection;
+
+public interface CustomerDao {
+
+
+    void addCustomer(Customer customer);
+
+    void deleteCustomer(Customer customer);
+
+    Customer getByField(String fieldName, String fieldValue);
+
+    Collection<Customer> getCustomers(String fieldName, String fieldValue);
+
+    Collection<Customer> getCustomers();
+
+    void updateCustomer(Customer customer);
+
+    void updateField(Customer customer, String fieldName, Object fieldValue);
+
+}
