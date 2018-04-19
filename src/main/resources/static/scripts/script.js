d8b93df20394c40c4abcc8ea947f642c6c7df72a
vv0rkman <b1kmoop@gmail.com>
2018-04-19 23:17:58 +0300

    Added close-action for registration form

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,10 +1,14 @@
-var modal = document.getElementById('id01');
+var modal1 = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }else
+        if(event.target == modal2){
+            modal2.style.display = "none";
+        }
 }
 
 
