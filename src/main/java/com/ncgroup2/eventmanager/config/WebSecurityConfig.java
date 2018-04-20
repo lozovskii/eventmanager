2a8557deff101e1550db75f6c5f622823a8aa835
vv0rkman <b1kmoop@gmail.com>
2018-04-20 21:49:10 +0300

    Removed unnecessary code

--- src/main/java/com/ncgroup2/eventmanager/config/WebSecurityConfig.java
+++ src/main/java/com/ncgroup2/eventmanager/config/WebSecurityConfig.java
@@ -32,8 +32,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests().antMatchers("/").permitAll();
 
-        http.authorizeRequests().antMatchers("/register").permitAll();
-
         http.authorizeRequests().antMatchers("/login","logout").anonymous();
 
 //        http.authorizeRequests().antMatchers("/register", "/registration**", "registrationConfirm").anonymous();
