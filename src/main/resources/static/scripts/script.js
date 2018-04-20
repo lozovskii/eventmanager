b84906b40cb234dae9e22662a63227175201ecf8
vv0rkman <b1kmoop@gmail.com>
2018-04-20 19:45:18 +0300

    Removed code for modal2 form

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,15 +1,11 @@
 var modal1 = document.getElementById('id01');
-var modal2 = document.getElementById('id02');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal1) {
         modal1.style.display = "none";
-    }else
-        if(event.target == modal2){
-            modal2.style.display = "none";
-        }
-}
+    }
+};
 
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -19,5 +15,5 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
-    });
+    })
 });
