696a97d8ff33879ce9adc59c3076a9b5584140b0
vv0rkman <b1kmoop@gmail.com>
2018-04-22 20:46:29 +0300

    Changed loginPage destination

--- src/main/java/com/ncgroup2/eventmanager/config/WebSecurityConfig.java
+++ src/main/java/com/ncgroup2/eventmanager/config/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .and()
                     .formLogin()
-                        .loginPage("/")
+                        .loginPage("/login")
                 .failureUrl("/?error")
                 .defaultSuccessUrl("/profile")
                 .and()
