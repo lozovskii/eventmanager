b96a796eba9850e4d3c4f69e76229c077fca236d
vv0rkman <b1kmoop@gmail.com>
2018-04-24 19:14:38 +0300

    Minor bug fixes

--- src/main/java/com/ncgroup2/eventmanager/dao/DAO.java
+++ src/main/java/com/ncgroup2/eventmanager/dao/DAO.java
@@ -1,7 +1,6 @@
 package com.ncgroup2.eventmanager.dao;
 
 import java.util.Collection;
-import com.ncgroup2.eventmanager.entity.Entity;
 
 public interface DAO <E, K> {
 
@@ -13,13 +12,13 @@ public interface DAO <E, K> {
 
     Collection<E> getEntitiesByField(String fieldName, K fieldValue);
 
-    void updateEntity(Entity entity);
+    void updateEntity(E entity);
 
     void updateEntityField(K id, String fieldName, K fieldValue);
 
     void deleteEntity(K id);
 
-    void createEntity(Entity entity);
+    void createEntity(E entity);
 
 
 }
