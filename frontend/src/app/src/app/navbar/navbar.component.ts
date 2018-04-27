61cb8731112ead5af157ad5fcbb47343e42c2cd9
Nadia Tiutiunyk <nadiatuytuynik96@gmail.com>
2018-04-27 13:44:26 +0300

    refactored functionality of buttons "log in" "logout"

--- frontend/src/app/src/app/navbar/navbar.component.ts
+++ frontend/src/app/src/app/navbar/navbar.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../_models/user";
+import {UserService} from "../_services/user.service";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../_services/authentication.service";
 
 @Component({
+  moduleId: module.id.toString(),
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  navbar = false;
+  currentUser: User;
 
-  constructor() { }
+  constructor(authenticationService : AuthenticationService) {
+    this.navbar = authenticationService.checkingLog();
+    console.log('navbar = ' + this.navbar);
+  }
+
 
   ngOnInit() {
+
   }
 
 }
