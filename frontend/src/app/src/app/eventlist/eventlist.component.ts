42c5e2a8b78f79e9bddc47f47c6617b3ff7716eb
Nadia Tiutiunyk <nadiatuytuynik96@gmail.com>
2018-04-27 13:46:39 +0300

    added eventlist component

---
+++ frontend/src/app/src/app/eventlist/eventlist.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {EventService} from "../_services/event.service";
+import {Event} from "../_models/event";
+import {UserService} from "../_services/user.service";
+
+@Component({
+  selector: 'app-eventlist',
+  templateUrl: './eventlist.component.html',
+  styleUrls: ['./eventlist.component.css']
+})
+
+export class EventlistComponent implements OnInit {
+  events: Event[];
+  eventsForm: FormGroup;
+  constructor(private eventService: EventService,
+              private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.eventsForm = this.formBuilder.group({});
+  }
+
+  getEvents(): void {
+    this.eventService.getAllEvents()
+      .subscribe((events) => this.events = events);
+  }
+
+  getEventsByCustId(): void {
+    this.eventService.getEventsByCustId()
+      .subscribe((events) => this.events = events);
+  }
+}
