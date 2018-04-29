e2b8d4fb7acbe99376490e54910684751a8d3c14
andriy96s <shulepaa@gmail.com>
2018-04-29 13:41:46 +0300

    Refactored to REST controller

--- backend/src/main/java/com/ncgroup2/eventmanager/controller/PasswordResetController.java
+++ backend/src/main/java/com/ncgroup2/eventmanager/controller/PasswordResetController.java
@@ -1,109 +1,89 @@
 package com.ncgroup2.eventmanager.controller;
 
 import com.ncgroup2.eventmanager.entity.Customer;
-import com.ncgroup2.eventmanager.service.sender.Sender;
 import com.ncgroup2.eventmanager.service.entityservice.CustomerService;
+import com.ncgroup2.eventmanager.service.sender.Sender;
 import com.ncgroup2.eventmanager.service.sender.SubjectEnum;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@Controller
+@RestController
+@RequestMapping("/api/reset")
 public class PasswordResetController {
 
-    @Autowired
-    CustomerService customerService;
+    private final CustomerService customerService;
+
+    private final Sender mailSender;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    private Sender mailSender;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-
-    @RequestMapping(value = "/reset", method = RequestMethod.POST)
-    public String resetPassword(
-
-            Model model,
-            @RequestParam("reset_email") String reset_email
-            ) {
-
-        System.out.println(reset_email);
-
-        if(customerService.isEmailUnique(reset_email)) {
-
-            model.addAttribute("customer_not_found", true);
-            return "redirect:/?q=customer_not_found";
-
-        }else {
-
-            Customer customer = customerService.getCustomerByEmail(reset_email);
-
-            String token = UUID.randomUUID().toString();
-
-            customerService.createVerificationToken(customer, token);
-
-            mailSender.sendEmail(customer.getEmail(), SubjectEnum.RESET_PASSWORD, token);
-
-        } return "redirect:/?q=We_sent_confirmation_email";
+    public PasswordResetController(CustomerService customerService, Sender mailSender, PasswordEncoder passwordEncoder) {
+        this.customerService = customerService;
+        this.mailSender = mailSender;
+        this.passwordEncoder = passwordEncoder;
     }
 
-        @RequestMapping(value = "/resetPassword", method = RequestMethod.GET)
-        public String displayResetPasswordPage(
-                Model model,
-                @RequestParam ("token") String token) {
 
-            Customer customer = customerService.getCustomerByToken(token);
+    //    @RequestMapping(value = "/reset", method = RequestMethod.POST)
+    @GetMapping(value = "/sendLink")
+    public ResponseEntity resetPassword(@RequestParam String email) {
 
-            if (customer == null) {
+        System.out.println(email);
 
-                model.addAttribute("customer_not_found", true);
-
-                return "redirect:/?q=customer_not_found";
-            }
-
-            model.addAttribute("token", token);
-
-            return "reset/reset_password";
-
+        if (customerService.isEmailUnique(email)) {
+            return new ResponseEntity("This email is not valid", HttpStatus.BAD_REQUEST);
         }
 
-    @RequestMapping(value = "/setNewPassword", method = RequestMethod.POST)
-    public String setPassword(
-            Model model,
-            @RequestParam ("password") String password,
-            @RequestParam ("token") String token)
-    {
+        Customer customer = customerService.getCustomerByEmail(email);
+
+        String token = UUID.randomUUID().toString();
+
+        customerService.createVerificationToken(customer, token);
+
+        mailSender.sendEmail(customer.getEmail(), SubjectEnum.RESET_PASSWORD, token);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    //    @RequestMapping(value = "/resetPassword", method = RequestMethod.GET)
+    @GetMapping(value = "/resetPassword")
+    public ResponseEntity displayResetPasswordPage(@RequestParam("token") String token) {
 
         Customer customer = customerService.getCustomerByToken(token);
 
-        System.out.println(token);
+        if (customer == null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(HttpStatus.OK);
 
-        System.out.println(customer.getEmail());
+    }
+
+    @RequestMapping(value = "/setNewPassword", method = RequestMethod.POST)
+    public ResponseEntity setPassword(
+            @RequestParam("password") String password,
+            @RequestParam("token") String token) {
+
+        Customer customer = customerService.getCustomerByToken(token);
+
+//        System.out.println(token);
+//
+//        System.out.println(customer.getEmail());
 
         if (customer == null) {
 
-            model.addAttribute("customer_not_found", true);
-
-            return "redirect:/?q=customer_not_found";
-
-        } else{
-
-
-            customer.setPassword(passwordEncoder.encode(password));
-
-            customerService.updatePassword(customer);
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
 
         }
 
+        customer.setPassword(passwordEncoder.encode(password));
 
-        return "redirect:/";
+        return new ResponseEntity(HttpStatus.OK);
 
     }
 
