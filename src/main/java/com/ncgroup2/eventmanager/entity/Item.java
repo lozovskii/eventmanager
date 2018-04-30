732e13eb7a39d61a04ff0e43b5983bb08f6cd1b0
vv0rkman <b1kmoop@gmail.com>
2018-04-30 13:53:20 +0300

    Added lombok

--- src/main/java/com/ncgroup2/eventmanager/entity/Item.java
+++ src/main/java/com/ncgroup2/eventmanager/entity/Item.java
@@ -1,62 +1,24 @@
 package com.ncgroup2.eventmanager.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Item extends Entity {
 
     private String description;
     private String image;
     private String link;
 
-    public Item() {
-    }
-
-    public Item(String name,String description, String image, String link) {
-        this.name = name;
-        this.description = description;
-        this.image = image;
-        this.link = link;
-    }
-
     @Override
     public Object[] getParams() {
         return new Object[]{
+                this.getId(),
                 this.name,
                 this.description,
                 this.image,
                 this.link
         };
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Item{" +
-                "description='" + description + '\'' +
-                ", image='" + image + '\'' +
-                ", link='" + link + '\'' +
-                '}';
     }
 }
