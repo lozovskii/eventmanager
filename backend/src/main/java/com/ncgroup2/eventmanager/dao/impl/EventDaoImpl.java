5fd02d4559875b6d729f8b789e01711a1f601cd5
Nadia Tiutiunyk <nadiatuytuynik96@gmail.com>
2018-05-02 00:38:56 +0300

    refactored

--- backend/src/main/java/com/ncgroup2/eventmanager/dao/impl/EventDaoImpl.java
+++ backend/src/main/java/com/ncgroup2/eventmanager/dao/impl/EventDaoImpl.java
@@ -129,7 +129,6 @@ public class EventDaoImpl extends JdbcDaoSupport implements EventDao {
 
     @Override
     public List getEventsByCustId(String custId) {
-        System.out.println(custId);
         String sql = "SELECT \"Event\".name AS name, start_time, end_time, \"Event\".description AS description, " +
                 "\"Event_Visibility\".name AS visibility, \"Event_Status\".name AS status " +
                 "FROM (\"Event\" INNER JOIN \"Event_Visibility\" " +
