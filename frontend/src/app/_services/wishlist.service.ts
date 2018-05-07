a91d1ad88237a6d591af627ec2c694af66f099e9
vv0rkman <b1kmoop@gmail.com>
2018-05-07 22:15:19 +0300

    Created method "isBooker" and "update"

--- frontend/src/app/_services/wishlist.service.ts
+++ frontend/src/app/_services/wishlist.service.ts
@@ -14,6 +14,10 @@ export class WishListService {
               private userService: UserService) {
   }
 
+  isBooker(bookerId : string): boolean{
+    return bookerId == this.userService.getCurrentId();
+  }
+
   getByEventId(eventId: string): Observable<WishList> {
     const url = `${this.wishListUrl}/${eventId}`;
     return this.http.get<WishList>(url,{headers: AuthenticationService.getAuthHeader()});
@@ -23,5 +27,10 @@ export class WishListService {
     let customerId = this.userService.getCurrentId();
     const url = `${this.wishListUrl}/booked?customerId=${customerId}`;
     return this.http.get<WishList>(url,{headers: AuthenticationService.getAuthHeader()});
+  }
+
+  update(wishList: WishList){
+    const url = `${this.wishListUrl}/update`;
+    return this.http.post<WishList>(url, wishList, {headers: AuthenticationService.getAuthHeader()});
   }
 }
