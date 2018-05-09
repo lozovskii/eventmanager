245fecf12c3d7685870ce99429cd51dadb1057ca
vv0rkman <b1kmoop@gmail.com>
2018-05-09 15:00:00 +0300

    Added tools for view created items

---
+++ frontend/src/app/wishlist/createdItems/createditems.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {AlertService} from "../../_services/alert.service";
+import {WishListService} from "../../_services/wishlist.service";
+import {Item} from "../../_models/item";
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-bookeditems',
+  templateUrl: './createditems.component.html',
+  styleUrls: ['../wishlist.component.css']
+})
+export class CreatedItemsComponent implements OnInit {
+  items : Item[];
+  hasChanges: boolean = false;
+
+  constructor(private wishListService: WishListService,
+              private alertService : AlertService,
+              private location: Location
+  ) {
+  }
+
+  ngOnInit() {
+      this.getCreatedItems();
+  }
+
+  getCreatedItems(): void {
+    this.wishListService.getCreatedItems()
+      .subscribe((items) => {
+        this.items = items;
+        }, (error) => {
+
+        this.location.back();
+
+        this.alertService.info('Items not found',true);
+      });
+  }
+}
