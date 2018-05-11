ae84fc3bc2eab6195284339479fb08cb4410bd63
Mykola <filimonovkolya97@gmail.com>
2018-05-11 15:40:38 +0300

    calendar add event list near calendar

---
+++ frontend/src/app/home/calendar/calendar.component.ts
@@ -0,0 +1,146 @@
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import * as moment from 'moment';
+import * as _ from 'lodash';
+import {EventService} from "../../_services";
+import {Event} from "../../_models";
+import {ActivatedRoute} from "@angular/router";
+import {AlertService} from "../../_services/alert.service";
+
+export interface CalendarDate {
+  mDate: moment.Moment;
+  selected?: boolean;
+  today?: boolean;
+}
+
+@Component({
+  selector: 'yoshimi-calendar',
+  templateUrl: './calendar.component.html',
+})
+export class CalendarComponent implements OnInit, OnChanges {
+
+  currentDate = moment();
+  dayNames = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+  weeks: CalendarDate[][] = [];
+  sortedDates: CalendarDate[] = [];
+  events: Event[];
+
+  @Input() selectedDates: CalendarDate[] = [];
+  @Output() onSelectDate = new EventEmitter<CalendarDate>();
+
+  constructor(private eventService: EventService,
+              private activatedRoute: ActivatedRoute,
+              private alertService: AlertService) {
+
+    this.eventService.getEventsByCustId().subscribe(
+      events => {
+
+        localStorage.setItem('Event', JSON.stringify(events));
+        console.log(events);
+        console.log(localStorage.getItem('Event'));
+
+      }
+    )
+
+  }
+
+  ngOnInit(): void {
+    this.generateCalendar();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.selectedDates &&
+      changes.selectedDates.currentValue &&
+      changes.selectedDates.currentValue.length > 1) {
+      // sort on date changes for better performance when range checking
+      this.sortedDates = _.sortBy(changes.selectedDates.currentValue, (m: CalendarDate) => m.mDate.valueOf());
+      this.generateCalendar();
+    }
+  }
+  getEvents(): void {
+    this.eventService.getAllEvents()
+      .subscribe((events) => {
+        this.events = events;
+        if(events.toString() == ''){
+          this.alertService.info('No events exist yet.',true);
+        }
+      });
+  }
+
+  // date checkers
+
+  isToday(date: moment.Moment): boolean {
+    return moment().isSame(moment(date), 'day');
+  }
+
+  isSelected(date: moment.Moment): boolean {
+    return _.findIndex(this.selectedDates, (selectedDate) => {
+      return moment(date).isSame(selectedDate.mDate, 'day');
+    }) > -1;
+  }
+
+  isSelectedMonth(date: moment.Moment): boolean {
+    return moment(date).isSame(this.currentDate, 'month');
+  }
+
+  selectDate(date: CalendarDate): void {
+    this.onSelectDate.emit(date);
+  }
+
+  // actions from calendar
+
+  prevMonth(): void {
+    this.currentDate = moment(this.currentDate).subtract(1, 'months');
+    this.generateCalendar();
+  }
+
+  nextMonth(): void {
+    this.currentDate = moment(this.currentDate).add(1, 'months');
+    this.generateCalendar();
+  }
+
+  firstMonth(): void {
+    this.currentDate = moment(this.currentDate).startOf('year');
+    this.generateCalendar();
+  }
+
+  lastMonth(): void {
+    this.currentDate = moment(this.currentDate).endOf('year');
+    this.generateCalendar();
+  }
+
+  prevYear(): void {
+    this.currentDate = moment(this.currentDate).subtract(1, 'year');
+    this.generateCalendar();
+  }
+
+  nextYear(): void {
+    this.currentDate = moment(this.currentDate).add(1, 'year');
+    this.generateCalendar();
+  }
+
+  // generate the calendar grid
+
+  generateCalendar(): void {
+    const dates = this.fillDates(this.currentDate);
+    const weeks: CalendarDate[][] = [];
+    while (dates.length > 0) {
+      weeks.push(dates.splice(0, 7));
+    }
+    this.weeks = weeks;
+  }
+
+  fillDates(currentMoment: moment.Moment): CalendarDate[] {
+    const firstOfMonth = moment(currentMoment).startOf('month').day();
+    const firstDayOfGrid = moment(currentMoment).startOf('month').subtract(firstOfMonth, 'days');
+    const start = firstDayOfGrid.date() + 1;
+    return _.range(start, start + 42)
+      .map((date: number): CalendarDate => {
+        const d = moment(firstDayOfGrid).date(date);
+        return {
+          today: this.isToday(d),
+          selected: this.isSelected(d),
+          mDate: d
+        };
+      });
+  }
+}
