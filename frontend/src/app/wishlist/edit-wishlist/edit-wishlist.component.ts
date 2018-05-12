a5dac0c51915d962c7cb7eda15f293476c0748d9
vv0rkman <b1kmoop@gmail.com>
2018-05-12 22:33:12 +0300

    Wish list. Style and logic improvements.

---
+++ frontend/src/app/wishlist/edit-wishlist/edit-wishlist.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {WishListService} from "../../_services/wishlist.service";
+import {WishList} from "../../_models/wishlist";
+
+@Component({
+  selector: 'app-edit-items',
+  templateUrl: './edit-wishlist.component.html',
+  styleUrls: ['../wishlist.component.css']
+})
+export class EditWishListComponent implements OnInit {
+
+  wishList: WishList;
+
+  constructor(private wishListService: WishListService) { }
+
+  ngOnInit() {
+    this.wishListService.currentWishList.subscribe((wishList) => this.wishList = wishList);
+  }
+}
