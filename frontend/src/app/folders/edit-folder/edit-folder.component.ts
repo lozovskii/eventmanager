2d885ecd45841d30d56111a5079a50e0f79cb8ad
Nadiia Tiutiunyk <nadiatuytuynik96@gmail.com>
2018-05-25 16:50:58 +0300

    added new component: edit-folder

---
+++ frontend/src/app/folders/edit-folder/edit-folder.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {FolderService} from "../../_services/folder.service";
+import {AlertService} from "../../_services/alert.service";
+import {Folder} from "../../_models/folder";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-edit-folder',
+  templateUrl: './edit-folder.component.html',
+  styleUrls: ['./edit-folder.component.css']
+})
+export class EditFolderComponent implements OnInit {
+  folders: Folder[];
+  folder:Folder;
+  folderForm: FormGroup;
+  constructor(private router: Router,
+              private alertService: AlertService,
+              private activatedRoute: ActivatedRoute,
+              private formBuilder: FormBuilder,
+              private folderService: FolderService) { }
+
+  ngOnInit(): void {
+    this.folderForm = this.initFolderForm();
+    this.getFoldersByCustId()
+  }
+
+  initFolderForm(): FormGroup {
+    return this.formBuilder.group({
+      id : new FormControl(),
+      name: new FormControl()
+    })
+  }
+
+  getFoldersByCustId(): void {
+    this.folderService.getAllFolders()
+      .subscribe((folders) => {
+        this.folders = folders;
+        if(folders.toString() == ''){
+          this.alertService.info('No folders exist yet.',true);
+        }
+      });
+  }
+
+  updateFolder(folder:Folder): void{
+    console.log(JSON.stringify(folder));
+    this.folderService.updateFolderName(folder.id, folder.name).subscribe(()=>{
+      this.alertService.info('Folder name successfully updated!',true);
+      });
+  }
+
+}
