d5cf73b024720195dd77f8fa21ecf27226e59c95
endryu1994 <endryu-kibenko@outlook.com>
2018-05-30 22:20:06 +0300

    Fixed bugs

--- backend/src/main/java/com/ncgroup2/eventmanager/entity/Relationship.java
+++ backend/src/main/java/com/ncgroup2/eventmanager/entity/Relationship.java
@@ -3,12 +3,14 @@ package com.ncgroup2.eventmanager.entity;
 public class Relationship extends Entity{
 
     private String second_name;
+    private String login;
 
     @Override
     public Object[] getParams() {
         return new Object[]{
                 getName(),
                 getSecond_name(),
+                getLogin(),
                 getId()
         };
     }
@@ -19,5 +21,13 @@ public class Relationship extends Entity{
 
     public void setSecond_name(String second_name) {
         this.second_name = second_name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
     }
 }
